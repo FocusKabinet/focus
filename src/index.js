ffe2f316fd59d8d7fcacd40aa6ecfc1fcdd1db6f
Viet Nguyen <vmnguyen@ryerson.ca>
2021-05-17 22:00:25 -0400

    setup intial firebase config

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { firestore, auth } from './app/firebase';
 
 ReactDOM.render(
   <React.StrictMode>
