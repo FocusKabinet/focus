b911b9727c773b3ab8340c6b980808d1b3d8d15b
Viet Nguyen <vmnguyen@ryerson.ca>
2021-05-17 22:00:25 -0400

    setup intial firebase config

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { firestore, auth } from './app/firebase';
 
 ReactDOM.render(<App />,  document.getElementById('root'));
 
