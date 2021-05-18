a0dd55bc48bdd5167157cd2d259c27c6eec081c4
Viet Nguyen <vmnguyen@ryerson.ca>
2021-05-18 17:25:41 -0400

    move pages to correct folders

--- src/App.js
+++ src/App.js
@@ -1,44 +1,47 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
-import Landing from './components/Landing';
-import Login from './components/Login';
-import Register from './components/Register';
-import Home from './components/Home';
-import Profile from './components/Profile';
-import Settings from './components/Settings';
-import Datapage from './components/Datapage';
-
+import Landing from './pages/Landing';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Settings from './pages/Settings';
+import Datapage from './pages/Datapage';
+import KabinetDashboard from './pages/KabinetDashboard';
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
           <Route exact path="/">
-              <Landing />
+            <Landing />
           </Route>
           <Route path="/login">
-              <Navbar title={"Login"} loggedIn={false}/>
-              <Login />
+            <Navbar title={'Login'} loggedIn={false} />
+            <Login />
           </Route>
           <Route path="/register" loggedIn={false}>
-              <Navbar title={"Register"} />
-              <Register />
+            <Navbar title={'Register'} />
+            <Register />
+          </Route>
+          <Route path="/kabinet-home">
+            <KabinetDashboard />
           </Route>
           <Route path="/:id" loggedIn={true}>
-              <Navbar title={"Home"}/>
-              <Home />
+            <Navbar title={'Home'} />
+            <Home />
           </Route>
           <Route path="/profile:id">
-              <Navbar title={"Profile"} loggedIn={true}/>
-              <Profile />
+            <Navbar title={'Profile'} loggedIn={true} />
+            <Profile />
           </Route>
           <Route path="/settings:id">
-              <Navbar title={"Settings"} loggedIn={true}/>
-              <Settings />
+            <Navbar title={'Settings'} loggedIn={true} />
+            <Settings />
           </Route>
           <Route path="/datapage:id">
-              <Navbar title={"Data"} loggedIn={true}/>
-              <Datapage />
+            <Navbar title={'Data'} loggedIn={true} />
+            <Datapage />
           </Route>
         </Switch>
       </div>
