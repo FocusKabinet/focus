91d398a9354f7a154b49f370c40cc84fd4059976
DenzelE <denzel@steamlabs.ca>
2021-05-18 12:33:29 -0400

    setting up initial paths

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Landing from './components/Landing';
+import Login from './components/Login';
+import Register from './components/Register';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import Settings from './components/Settings';
+import Datapage from './components/Datapage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+              <Landing />
+          </Route>
+          <Route path="/login">
+              <Navbar title={"Login"} loggedIn={false}/>
+              <Login />
+          </Route>
+          <Route path="/register" loggedIn={false}>
+              <Navbar title={"Register"} />
+              <Register />
+          </Route>
+          <Route path="/:id" loggedIn={true}>
+              <Navbar title={"Home"}/>
+              <Home />
+          </Route>
+          <Route path="/profile:id">
+              <Navbar title={"Profile"} loggedIn={true}/>
+              <Profile />
+          </Route>
+          <Route path="/settings:id">
+              <Navbar title={"Settings"} loggedIn={true}/>
+              <Settings />
+          </Route>
+          <Route path="/datapage:id">
+              <Navbar title={"Data"} loggedIn={true}/>
+              <Datapage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Datapage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Datapage() {
+    return (
+        <div>
+            Data
+        </div>
+    )
+}
+
+export default Datapage
