35573f64ee1920faf174e818d6e47d40e89e016d
DenzelE <denzel@steamlabs.ca>
2021-05-18 12:33:29 -0400

    setting up initial paths

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import '../../styles/navbar.scss';
+import Nav from './Nav';
+
+function NavBar({title,...props}) {
+  return (
+    <header className="navbar">
+      <Nav title={title}/>
+      <h1>{title}</h1>
+    </header>
+  );
+}
+
+export default NavBar;
