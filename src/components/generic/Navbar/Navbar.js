fa8ff62436739cbfc390e12c95622c99d82ab393
DenEdwards <denzelsedwards@gmail.com>
2021-05-24 20:13:43 -0400

    Conflict Resolution Final

--- src/components/generic/Navbar/Navbar.js
+++ src/components/generic/Navbar/Navbar.js
@@ -2,18 +2,20 @@ import '../../styles/navbar.scss';
 import Nav from './Nav';
 
 function NavBar(props) {
-  if(props.loggedIn){
-    return (
-      <header className="navbar">
-        <Nav title={props.title}/>
-        <h1>{props.title}</h1>
-      </header>
-    );
-  }else{return(
-    <header className="navbar">
-        <h1>Focus</h1>
-    </header>
-  );}
+	if (props.loggedIn) {
+		return (
+			<header className='navbar'>
+				<Nav title={props.title} />
+				<h1>{props.title}</h1>
+			</header>
+		);
+	} else {
+		return (
+			<header className='navbar'>
+				<h1>Focus</h1>
+			</header>
+		);
+	}
 }
 
 export default NavBar;
