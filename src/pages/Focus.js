366acf2ced488ddef4a6b27753f366b3d3187b99
DenEdwards <denzelsedwards@gmail.com>
2021-05-25 22:27:12 -0400

    Created Data Popup

--- src/pages/Focus.js
+++ src/pages/Focus.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './styles/Focus.scss';
-import { Grid, Typography, Button } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import Timer from '../components/Focus/Timer';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-	faTasks,
-	faHourglassHalf,
-	faCog,
-} from '@fortawesome/free-solid-svg-icons';
+import DataPopup from '../components/Focus/DataPopup';
 
 function Focus({ handleLogout, ...props }) {
 	const [background, setBackground] = React.useState({ backgroundColor: '' });
+	const [dataVisible, setDataVisible] = React.useState(false);
 
 	const changeBackground = (color) => {
 		setBackground({ backgroundColor: color });
@@ -19,33 +15,28 @@ function Focus({ handleLogout, ...props }) {
 	return (
 		<div className='page-container' style={background}>
 			<Grid container justify='center' alignItems='center' spacing={5}>
-				<Grid
-					container
-					direction='row'
-					justify='flex-end'
-					alignItems='center'
-					item
-					spacing={1}
-					xs={12}
-				>
-					<Grid item xs={10}>
-						<Typography variant='h2'>Timer Title</Typography>
-					</Grid>
-					<Grid item xs={1}>
-						<Button variant='contained' size='medium'>
-							<FontAwesomeIcon icon={faTasks} className='icons' /> Tasks
-						</Button>
-					</Grid>
-					<Grid item xs={1}>
-						<Button variant='contained' size='medium'>
-							<FontAwesomeIcon icon={faHourglassHalf} className='icons' /> Data
-						</Button>
-					</Grid>
+				<Grid item xs={12}>
+					<Typography variant='h2'>Pomodoro</Typography>
 				</Grid>
-				<Grid item xs={6}>
-					<Timer changePageBackground={changeBackground} />
+				<Grid
+					item
+					xs={12}
+					container
+					justify='center'
+					alignItems='center'
+					direction='row'
+					sapcing={3}
+				>
+					<Grid item xs={6}>
+						<Timer
+							visible={dataVisible}
+							setVisible={setDataVisible}
+							changePageBackground={changeBackground}
+						/>
+					</Grid>
 				</Grid>
 			</Grid>
+			{/* <DataPopup visible={dataVisible} setVisible={setDataVisible} /> */}
 			<button onClick={handleLogout}>logout</button>
 		</div>
 	);
