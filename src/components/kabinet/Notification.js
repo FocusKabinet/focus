5a97d154a3de590fb36585b931640fe4d8cc3515
Viet Nguyen <vmnguyen@ryerson.ca>
2021-06-03 03:15:53 -0400

    kabinet redux notification v1

---
+++ src/components/kabinet/Notification.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import Snackbar from '@material-ui/core/Snackbar';
+import { connect } from 'react-redux';
+import { setNotificationState } from '../../redux/actions/notification';
+import { IconButton, Slide } from '@material-ui/core';
+import {
+  CheckCircleOutline,
+  Close,
+  ErrorOutline,
+  InfoOutlined,
+  WarningRounded,
+} from '@material-ui/icons';
+import './styles/Notification.scss';
+
+function Snackbars(props) {
+  const vertical = 'top';
+  const horizontal = 'left';
+  const config = { vertical, horizontal };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    props.close();
+  };
+
+  return (
+    <div>
+      <Snackbar
+        anchorOrigin={config}
+        open={props.open}
+        onClose={handleClose}
+        message={Message(props)}
+        autoHideDuration={3000}
+        TransitionComponent={Transition}
+        action={
+          <React.Fragment>
+            <IconButton
+              size="small"
+              aria-label="close"
+              color="inherit"
+              onClick={handleClose}
+            >
+              <Close fontSize="small" />
+            </IconButton>
+          </React.Fragment>
+        }
+        ContentProps={{ className: props.severity }}
+      />
+    </div>
+  );
+}
+
+function Message(props) {
+  let icon = null;
+
+  switch (props.severity) {
+    case 'success':
+      icon = <CheckCircleOutline />;
+      break;
+    case 'error':
+      icon = <ErrorOutline />;
+      break;
+    case 'warning':
+      icon = <WarningRounded />;
+      break;
+    default:
+      icon = <InfoOutlined />;
+  }
+
+  return (
+    <div className="noti-message">
+      {icon}
+      {props.message}
+    </div>
+  );
+}
+function Transition(props) {
+  return <Slide {...props} direction="down" />;
+}
+
+const mapStateToProps = (state) => {
+  return {
+    ...state.notification,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    close: () => dispatch(setNotificationState({ open: false })),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Snackbars);
--- src/helpers/kabinetHelpers.js
+++ src/helpers/kabinetHelpers.js
@@ -1,63 +1,142 @@
-import data from './kabinetMockData';
 import { firestore, storageRef } from '../app/firebase';
 import axios from 'axios';
 import { projectStore as store } from '../redux/store';
 import { setLoadingState } from '../redux/actions/loading';
+import { setNotificationState } from '../redux/actions/notification';
 
 export async function fetchCards() {
-  store.dispatch(setLoadingState(true));
   let cards = [];
-  const cardsRef = firestore.collection('kabinet');
-  const cardsSnapshot = await cardsRef.orderBy('createdAt', 'desc').get();
-  cardsSnapshot.forEach((doc) => {
-    cards.push({ ...doc.data(), id: doc.id });
-  });
-  store.dispatch(setLoadingState(false));
-
+  try {
+    store.dispatch(setLoadingState(true));
+    const cardsRef = firestore.collection('kabinet');
+    const cardsSnapshot = await cardsRef.orderBy('createdAt', 'desc').get();
+    cardsSnapshot.forEach((doc) => {
+      cards.push({ ...doc.data(), id: doc.id });
+    });
+    store.dispatch(setLoadingState(false));
+  } catch (e) {
+    store.dispatch(setLoadingState(false));
+    console.error(e);
+    store.dispatch(
+      setNotificationState({
+        open: true,
+        message: 'Failed to load feed!',
+        severity: 'error',
+      })
+    );
+  }
   return cards;
 }
 
 export async function fetchCard(id) {
-  store.dispatch(setLoadingState(true));
   let card = {};
-  const cardRef = firestore.collection('kabinet').doc(id);
-  const cardSnapshot = await cardRef.get();
-  card = cardSnapshot.data();
-  store.dispatch(setLoadingState(false));
+  try {
+    store.dispatch(setLoadingState(true));
+    const cardRef = firestore.collection('kabinet').doc(id);
+    const cardSnapshot = await cardRef.get();
+    card = cardSnapshot.data();
+    store.dispatch(setLoadingState(false));
+  } catch (e) {
+    store.dispatch(setLoadingState(false));
+    console.error(e);
+    store.dispatch(
+      setNotificationState({
+        open: true,
+        message: 'Failed to load item!',
+        severity: 'error',
+      })
+    );
+  }
+
   return card;
 }
 
 export async function addCard(data, img) {
   store.dispatch(setLoadingState(true));
-  const snapshot = await firestore.collection('kabinet').add(data);
-  const id = snapshot.id;
+  try {
+    const snapshot = await firestore.collection('kabinet').add(data);
+    const id = snapshot.id;
 
-  if (img) {
-    const url = await uploadImage(id, img);
-    const res = await snapshot.update({ imageURL: url });
-    console.log(url);
+    if (img) {
+      const url = await uploadImage(id, img);
+      const res = await snapshot.update({ imageURL: url });
+    }
+    store.dispatch(setLoadingState(false));
+    store.dispatch(
+      setNotificationState({
+        open: true,
+        message: 'New card added successfully!',
+        severity: 'success',
+      })
+    );
+  } catch (e) {
+    store.dispatch(setLoadingState(false));
+    console.error(e);
+    store.dispatch(
+      setNotificationState({
+        open: true,
+        message: 'Failed to add new card!',
+        severity: 'error',
+      })
+    );
+    return 'error';
   }
-  store.dispatch(setLoadingState(false));
-  return snapshot;
 }
 
 export async function updateCard(id, data, img) {
-  store.dispatch(setLoadingState(true));
-  const cardsRef = firestore.collection('kabinet').doc(id);
-  const url = await uploadImage(id, img);
-  const res = url
-    ? await cardsRef.update({ ...data, imageURL: url })
-    : await cardsRef.update(data);
-  store.dispatch(setLoadingState(false));
-  return res;
+  try {
+    store.dispatch(setLoadingState(true));
+    const cardsRef = firestore.collection('kabinet').doc(id);
+    const url = await uploadImage(id, img);
+    const res = url
+      ? await cardsRef.update({ ...data, imageURL: url })
+      : await cardsRef.update(data);
+    store.dispatch(setLoadingState(false));
+    store.dispatch(
+      setNotificationState({
+        open: true,
+        message: 'Card updated successfully!',
+        severity: 'success',
+      })
+    );
+  } catch (e) {
+    store.dispatch(setLoadingState(false));
+    console.error(e);
+    store.dispatch(
+      setNotificationState({
+        open: true,
+        message: 'Failed to update card!',
+        severity: 'error',
+      })
+    );
+    return 'error';
+  }
 }
 
 export async function deleteCard(id) {
-  store.dispatch(setLoadingState(true));
-  deleteImage(id);
-  await firestore.collection('kabinet').doc(id).delete();
-  store.dispatch(setLoadingState(false));
-  return;
+  try {
+    store.dispatch(setLoadingState(true));
+    deleteImage(id);
+    await firestore.collection('kabinet').doc(id).delete();
+    store.dispatch(setLoadingState(false));
+    store.dispatch(
+      setNotificationState({
+        open: true,
+        message: 'Card deleted successfully!',
+        severity: 'success',
+      })
+    );
+  } catch (e) {
+    store.dispatch(setLoadingState(false));
+    console.error(e);
+    store.dispatch(
+      setNotificationState({
+        open: true,
+        message: 'Failed to delete card!',
+        severity: 'error',
+      })
+    );
+  }
 }
 
 async function uploadImage(id, img) {
@@ -76,6 +155,17 @@ async function uploadImage(id, img) {
         imageURL = url;
         console.log(imageURL);
       });
+    })
+    .catch((e) => {
+      store.dispatch(setLoadingState(false));
+      console.error(e);
+      store.dispatch(
+        setNotificationState({
+          open: true,
+          message: 'Failed to upload image!',
+          severity: 'error',
+        })
+      );
     });
   return imageURL;
 }
@@ -100,12 +190,19 @@ export async function getCurrentCountry() {
       const { countryCode, country } = res.data;
       code = { countryCode, country };
     })
-    .catch((e) =>
+    .catch((e) => {
       console.warn(
         'Cannot get location. Most likely due to AdBlock. Defaulted to US\n',
         e
-      )
-    );
+      );
+      store.dispatch(
+        setNotificationState({
+          open: true,
+          message: 'Failed to get location! Defaulted to US',
+          severity: 'warning',
+        })
+      );
+    });
   store.dispatch(setLoadingState(false));
   return code;
 }
@@ -115,7 +212,16 @@ export async function getGoogleTrends(countryCode) {
   let data = {};
   await axios('http://localhost:5000/api/' + countryCode)
     .then((response) => (data = response.data[0]))
-    .catch((e) => console.error(e));
+    .catch((e) => {
+      console.error(e);
+      store.dispatch(
+        setNotificationState({
+          open: true,
+          message: 'Failed to fetch Google Trends!',
+          severity: 'warning',
+        })
+      );
+    });
   store.dispatch(setLoadingState(false));
   return data;
 }
@@ -127,7 +233,16 @@ export async function getHeadLines(countryCode, page = 1, pageSize = 20) {
     `https://newsapi.org/v2/top-headlines?country=${countryCode}&pageSize=${pageSize}&page=${page}&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`
   )
     .then((res) => (data = res.data))
-    .catch((e) => console.error('News API error occured', e));
+    .catch((e) => {
+      console.error('News API error occured', e);
+      store.dispatch(
+        setNotificationState({
+          open: true,
+          message: 'Failed to fetch headlines!',
+          severity: 'warning',
+        })
+      );
+    });
   data['hasMore'] = data.totalResults - page * pageSize > 0;
   data['page'] = page;
   data['pageSize'] = pageSize;
