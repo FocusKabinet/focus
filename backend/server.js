a858d6574e15e35f3d5faad60e2c954ca6ea7cbc
Viet Nguyen <vies.law86@gmail.com>
2021-06-18 14:16:19 -0400

    reconfig server for deployment

--- backend/server.js
+++ backend/server.js
@@ -40,10 +40,12 @@ app.get('/api/trends/:countryCode', async (req, res) => {
     );
 });
 
-app.use(express.static(path.join(__dirname, '../build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
